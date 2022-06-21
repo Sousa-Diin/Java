f7b939430d38ecf38baf2cf410293281d2b5a307
Wildes Sousa <74382948+Sousa-Diin@users.noreply.github.com>
2022-06-21 00:48:50 -0300

    Update ContaBancaria_project

--- ContaBancaria/src/contabancaria/ContaBancaria.java
+++ ContaBancaria/src/contabancaria/ContaBancaria.java
@@ -9,19 +9,44 @@ public class ContaBancaria {
         ArrayList<Cliente> clientes = new ArrayList();
         
         Cliente cliente = new Cliente("Itau","wildes","poupanca",1473, 0743,'3');
-        Cliente cliente1 = new Cliente("Caixa","Dinho","corrente",129, 0743,'0');
+        Cliente cliente1 = new Cliente("Caixa","Wildes","corrente",129, 0743,'0');
         
         clientes.add(cliente);
         clientes.add(cliente1);
         
-        int qtd = 0;
+        int investidor = 0;
+        int correntista = 0;
+        int poupista = 0;
         
         for(Cliente c: clientes){
-            System.out.println(c.getBanco() + "-" + c.getTilular());
-            qtd = clientes.size();
+            System.out.println("Banco: " + c.getBanco() + "\nTitular: " + c.getTilular()+"\nTipo da conta: "+ c.getTipoConta() + "\n");
+            
+            if(c.getBanco().equals("poupanca")){
+                investidor += 1;
+            }
+            
+            if(c.getBanco().equals("corrente")){
+                correntista += 1;
+            }
+            else{
+                
+                investidor +=1;
+            }
         }
         
-        System.out.println(qtd);
+        
+        System.out.println("Total de clientes    : " + clientes.size() + "\n");
+        System.out.println("Total de correntistas: " + correntista + "\n");
+        System.out.println("Total de poupistas   : " + poupista + "\n");
+        System.out.println("Total de investidor   : " + investidor + "\n");
+        
+        //Remove indice do Array
+        /*clientes.remove(cliente);
+        
+        for(Cliente c: clientes){
+            System.out.println("Banco: " + c.getBanco() + "\nTitular: " + c.getTilular()+"\nTipo da conta: "+ c.getTipoConta() + "\n");
+        }*/
+        
     }
 
     
